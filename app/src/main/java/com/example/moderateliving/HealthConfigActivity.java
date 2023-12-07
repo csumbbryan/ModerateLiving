cbaf6185a47bd595424873df5b19b61e8786b63e
csumbbryan <bzanoli@csumb.edu>
2023-12-06 20:42:13 -0800

    Rolling back a couple changes.

---
+++ app/src/main/java/com/example/moderateliving/HealthConfigActivity.java
@@ -0,0 +1,14 @@
+package com.example.moderateliving;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthConfigActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_health_config);
+  }
+}
